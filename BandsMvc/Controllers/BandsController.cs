4420a62e1cb6487df835dd1fa081c24c9bfc271a
Tan Thai <Vi.Tan.Thai@gmail.com>
2023-08-07 16:45:26 +0200

    did the whole thing with code along

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,30 @@
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMvc.Controllers
+{
+	public class BandsController : Controller
+	{
+		DataService dataService;
+
+		public BandsController()
+		{
+			dataService = new DataService();
+		}
+
+		[HttpGet("")]
+		[HttpGet("index.php")]
+		public IActionResult Index()
+		{
+			var band = dataService.GetAllBands();
+			return View(band);
+		}
+
+		[HttpGet("detail/{id}")]
+		public IActionResult Detail(int id)
+		{
+			var band = dataService.GetBandById(id);
+			return Content($"ID:{band.Id}, Band:{band.Name}, Genre:{band.Description}");
+		}
+	}
+}
---
+++ BandsMvc/Models/DataService.cs
@@ -0,0 +1,26 @@
+﻿namespace BandsMvc.Models
+{
+	public class DataService
+	{
+		List<Band> bands = new List<Band>()
+		{
+			new Band { Id = 312, Name = "Zutomayo", Description = "JP Rock"},
+			new Band { Id = 543, Name = "Musical Youth", Description = "Reggae"},
+			new Band { Id = 33, Name = "N.W.A", Description = "Hip Hop"},
+		};
+
+		public Band GetBandById(int id)
+		{
+			return bands
+				.First(x => x.Id == id);
+		}
+
+		public Band[] GetAllBands()
+		{
+			return bands
+				.OrderBy(x => x.Name)
+				.ToArray();
+		}
+
+	}
+}
