1fc96475d22581356829356a98728fe63b96becb
Tan Thai <Vi.Tan.Thai@gmail.com>
2023-08-07 15:31:41 +0200

    Add project files.

---
+++ BandsMvc/Program.cs
@@ -0,0 +1,16 @@
+namespace BandsMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+            app.Run();
+        }
+    }
+}
